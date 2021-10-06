23b142f85361fc09e29f69a47d80c3a225a28411
KimYongKwon <dydrnjs518@gmail.com>
2021-10-06 19:05:39 +0900

    feat: 스트라이크와 볼의 갯수를 카운팅하는 Counter클래스 구현

---
+++ src/main/java/baseball/BaseballCounter.java
@@ -0,0 +1,45 @@
+package baseball;
+
+public class BaseballCounter {
+
+    public BaseballCounter() {
+    }
+
+    public int countStrike(String computerBalls, String userBalls) {
+        int strikeNum = 0;
+
+        for ( int i = 0; i < computerBalls.length(); ++i ) {
+            strikeNum += checkEqualBall(computerBalls.charAt(i), userBalls.charAt(i));
+        }
+
+        return strikeNum;
+    }
+
+    public int checkEqualBall(char computerBall, char userBall) {
+        if (computerBall == userBall)
+            return 1;
+        return 0;
+    }
+
+    public int countBall(String computerBall, String userBall) {
+        int ballNum = 0;
+
+        for ( int i = 0; i < computerBall.length(); ++i ) {
+            ballNum += checkEqualBallDifferIdx(i, computerBall.charAt(i), userBall);
+        }
+
+        return ballNum;
+    }
+
+    public int checkEqualBallDifferIdx(int computerBallIdx, char computerBall, String userBall) {
+        int ballNum = 0;
+
+        for ( int j = 0; j < userBall.length(); ++j ) {
+            if ( computerBallIdx != j ) {
+                ballNum += checkEqualBall(computerBall, userBall.charAt(j));
+            }
+        }
+
+        return ballNum;
+    }
+}
