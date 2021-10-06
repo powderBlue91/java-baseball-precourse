aea97597760f07760008c5b234f3efbdfc2dcdb9
KimYongKwon <dydrnjs518@gmail.com>
2021-10-06 19:06:57 +0900

    feat: 볼과 스트라이크 카운팅 결과를 통해 판정을 내려주는Judge 클래스 구현

---
+++ src/main/java/baseball/Judge.java
@@ -0,0 +1,54 @@
+package baseball;
+
+enum JudgeState {
+    STRIKE,
+    NOTHING,
+    ALL_STRIKE,
+    ALL_BALL,
+    NORMAL,
+    INPUT_ERROR
+}
+
+public class Judge {
+
+    static int strikeNum;
+    static int ballNum;
+
+    static BaseballCounter baseballCounter = new BaseballCounter();
+
+    static public JudgeState execute(String computerBalls, String userBalls) {
+
+        if (userBalls.equals("[ERROR]"))
+            return JudgeState.INPUT_ERROR;
+
+        return judgeBaseBall(baseballCounter.countStrike(computerBalls, userBalls),
+                baseballCounter.countBall(computerBalls, userBalls));
+    }
+
+    static public JudgeState judgeBaseBall(int strikes, int balls) {
+        strikeNum = strikes;
+        ballNum = balls;
+
+        if (strikeNum == 3)
+            return JudgeState.STRIKE;
+
+        if (strikeNum == 0 && ballNum == 0)
+            return JudgeState.NOTHING;
+
+        if (strikeNum > 0 && ballNum == 0)
+            return JudgeState.ALL_STRIKE;
+
+        if (strikeNum == 0 && ballNum > 0)
+            return JudgeState.ALL_BALL;
+
+        return JudgeState.NORMAL;
+    }
+
+    static public int getStrikeNum() {
+        return strikeNum;
+    }
+
+    static public int getBallNum() {
+        return ballNum;
+    }
+}
